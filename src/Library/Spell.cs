0277b4fe594164e5ce266a64243eb531186204bc
Ignacio Rodriguez <ignaciorodriguezsantos5@gmail.com>
2025-09-20 23:47:09 -0300

    Spell

---
+++ src/Library/Spell.cs
@@ -0,0 +1,20 @@
+﻿namespace Library;
+
+public class Spell
+{
+    public string Name { get; set; }
+    public string Type{ get; set; }
+    public int Healing{ get; set; }
+    public int ManaCost{ get; set; }
+    public int Damage{ get; set; }
+
+    public Spell(string name, string type, int healing, int manaCost, int damage)
+    {
+        Name = name;
+        Type = type;
+        Healing = healing;
+        ManaCost = manaCost;
+        Damage = damage;
+    }
+
+}
