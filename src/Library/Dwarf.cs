b5cef31920c2946d6ab6a27c5880379204385db3
Gaston <gardilgaston@gmail.com>
2025-09-21 00:37:06 -0300

    Implementacion de Dwarf

---
+++ src/Library/Dwarf.cs
@@ -0,0 +1,55 @@
+namespace Library;
+
+public class Dwarf
+{
+    public string Name { get; set; }
+    public int Health { get; set; }
+    public int Defense { get; set; }
+    public int Damage { get; set; }
+    
+    public Weapon Weapon { get; set; }
+    
+    public Armor Armor { get; set; }
+    
+    public Dwarf(string name, int health, int defense, int damage)
+        {
+        Name = name;
+        Health = 100;
+        Defense = 3;
+        Damage = 6;
+        }
+    
+    public void Attack(ICharacter character)
+    {
+        int totalDamage = Damage;
+        if (Weapon != null)
+        {
+            totalDamage += Weapon.Damage;
+        }
+        character.HP = totalDamage - character.Defense;
+    }
+    
+    public void Heal(ICharacter character)
+    {
+        character.HP = 100; 
+    }
+    
+    public void ChangeItem(IItem item)
+    {
+        if (item.Type == "Weapon")
+            Weapon = (Weapon)item;
+        else if (item.Type == "Armor")
+            Armor = (Armor)item;
+    }
+    
+    public void DropItem(IItem item)
+    {
+        if (item.Type == "Weapon" && Weapon == item)
+            Weapon = null;
+        else if (item.Type == "Armor" && Armor == item)
+            Armor = null;
+    }
+    
+
+    
+}
