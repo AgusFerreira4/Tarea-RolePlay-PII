8d7ef0e41a3ba9c8ffd20ac9dcd5d8cd18726fb7
Ignacio Rodriguez <ignaciorodriguezsantos5@gmail.com>
2025-09-20 23:16:34 -0300

    Weapon

--- src/Library/IItem.cs
+++ src/Library/IItem.cs
@@ -4,6 +4,7 @@ public interface IItem
 {
     public string Name { get; set; }
     public string Type { get; set; }
+    public int Defense { get; set; }
     public int Damage { get; set; }
     public int Healing { get; set; }
 }
---
+++ src/Library/Weapon.cs
@@ -0,0 +1,21 @@
+﻿namespace Library;
+
+public class Weapon : IItem
+{
+    public string Name { get; set; }
+    public string Type { get; set; }
+    public int Damage { get; set; }
+
+    public int Defense { get; set; }
+    public int Healing { get; set; }
+
+    public Weapon(string name, string type, int damage)
+    {
+        Name = name;
+        Type = type;
+        Damage = damage;
+        Defense = 0;
+        Healing = 0;
+
+    }
+}
